cda5ad856b0cfb904d74b7382321775bfa54264b
V Karthick Raja <91518791+vkarthickraja@users.noreply.github.com>
2025-02-05 21:49:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 
 // Set up scene, camera, and renderer
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById("bg"),
   antialias: true
